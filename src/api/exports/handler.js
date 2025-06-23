71cc4216b1b1959eff441ae863ac9e741e7ef27f
yahya.hafidz <yahya.hafidz@amarbank.co.id>
2025-06-23 20:57:00 +0700

    local storage implemented

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -1,7 +1,6 @@
-const ClientError = require("../../exceptions/ClientError");
+const ClientError = require('../../exceptions/ClientError');
 
 class ExportsHandler {
-  // eslint-disable-next-line no-useless-constructor
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
@@ -9,16 +8,19 @@ class ExportsHandler {
     this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
   }
 
-  async postExportNotesHandler(request, h) {
+  async postExportNotesHandler(request, h) { 
     this._validator.validateExportNotesPayload(request.payload);
+
     const message = {
       userId: request.auth.credentials.id,
       targetEmail: request.payload.targetEmail,
     };
-    await this._service.sendMessage("export:notes", JSON.stringify(message));
+
+    await this._service.sendMessage('export:notes', JSON.stringify(message));
+
     const response = h.response({
-      status: "success",
-      message: "Permintaan Anda dalam antrean",
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
     });
     response.code(201);
     return response;
